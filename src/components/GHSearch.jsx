660b2cf8e14cf04aa57feefdf69fdcca8fef8aab
felixoppongwillborg <fwlixw@gmail.com>
2020-08-05 16:54:56 +0200

    manage to display a result from github using axios

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Input, Form } from 'semantic-ui-react'
+import axios from 'axios'
 
-const GHSearch = () => {
-  return (
+class GHSearch extends Component {
+  state = {
+    result: [],
+
+  }
+
+  searchHandler = async event => {
+    let response;
+    const query = event.target.sfield.value
+      response = await axios.get(`https://api.github.com/search/users?q=${query}`)
+        this.setState({ result: response.data.items })
+    } 
+
+
+  renderResults = (items) => {
+    let a = items.map((item) => {
+      return(
+        <div>
+          <h3>{item.login}</h3>
+        </div>
+      )
+    })
+    return a
+  }
+
+  render(){
+    return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
+      <Form onSubmit={this.searchHandler}>
+        <Input type="text" id="search-field" name="sfield" placeholder="Input GH username"/>
+        <Button name="search-btn" id="search-submit">Search</Button>
+      </Form>
+        {this.renderResults(this.state.result)}
+      
+      
     </>
   )
+  }
 }
-
 export default GHSearch
