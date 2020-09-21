a0c740e647a285ea364a14add4c799f1479e60fc
felixoppongwillborg <fwlixw@gmail.com>
2020-09-21 15:16:40 +0200

    added axios and search fuction in GHsearch component

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,44 +1,45 @@
-import React, { Component } from 'react'
-import { Button, Input, Form } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Input, Button } from 'semantic-ui-react'
 import axios from 'axios'
 
 class GHSearch extends Component {
   state = {
-    result: [],
-
+    search: '',
+    items: []
+  }
+  async performSearch() {
+    const results = await axios.get(`https://api.github.com/search/users?q=${this.state.search}`)
+    this.setState({ items: results.data.items })
+  }
+  
+  setValue = (event) => {
+    this.setState({ search: event.target.value })
   }
 
-  searchHandler = async event => {
-    let response;
-    const query = event.target.sfield.value
-      response = await axios.get(`https://api.github.com/search/users?q=${query}`)
-        this.setState({ result: response.data.items })
-    } 
-
-
-  renderResults = (items) => {
-    let a = items.map((item) => {
-      return(
-        <div>
-          <h3>{item.login}</h3>
-        </div>
+  render() {
+    let itemsDisplay = this.state.items.map(item => {
+      return (
+        <li>{item.login}</li>
       )
     })
-    return a
-  }
-
-  render(){
     return (
-    <>
-      <Form onSubmit={this.searchHandler}>
-        <Input type="text" id="search-field" name="sfield" placeholder="Input GH username"/>
-        <Button name="search-btn" id="search-submit">Search</Button>
-      </Form>
-        {this.renderResults(this.state.result)}
-      
-      
-    </>
-  )
+      <>
+        <Input
+          onChange={e => this.setValue(e)}
+          type="text"
+          name="search"
+          placeholder="Input GH username"
+        />
+        <Button
+          name="search"
+          onClick={this.performSearch.bind(this)}>
+          Search
+        </Button>
+        <ul id="search-results">
+          {itemsDisplay}
+        </ul>
+      </>
+    );
   }
 }
-export default GHSearch
+export default GHSearch;
